2322c5521a13116e8d913e6f4e912ec56986ccbb
missionbaynorth <alice.zhang@mypocketcfo.com>
2025-05-31 21:29:37 -0700

    Fix Tailwind color configuration for ring utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,9 +15,7 @@ export default {
         'neon': {
           DEFAULT: '#00FF94',
         },
-        'neon-translucent': {
-          DEFAULT: 'rgba(0, 255, 148, 0.5)'
-        },
+        'neon-translucent': 'rgba(0, 255, 148, 0.5)',
         'metallic': {
           DEFAULT: '#FFD700',
           translucent: 'rgba(255, 215, 0, 0.4)'
